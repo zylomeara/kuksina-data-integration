c564aa4ac9022ff369c514a0dc08d539850ad100
Бабак Артем <aababak@sberbank.ru>
2020-11-11 21:23:06 +0300

    fix

--- src/main/java/beans/CustomerCsvWriter.java
+++ src/main/java/beans/CustomerCsvWriter.java
@@ -19,7 +19,7 @@ public class CustomerCsvWriter {
 
     public void csvWriterOneByOne(List<String[]> rows, String path) throws IOException {
         var appendEnabled = true;
-        var writer = new CSVWriter(new FileWriter(path, appendEnabled));
+        var writer = new CSVWriter(new FileWriter(path, appendEnabled), CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
         rows.forEach(writer::writeNext);
         writer.close();
     }
@@ -32,6 +32,7 @@ public class CustomerCsvWriter {
                 String.valueOf(customer1.getTrafficKB()),
                 customer1.getDate(),
                 customer1.getCity(),
+                String.valueOf(customer1.getStreet()),
                 String.valueOf(customer1.getStreetNumber()),
         }).collect(Collectors.toList());
         csvWriterOneByOne(rows, pathToCustomerDir + "/" + fileName);
